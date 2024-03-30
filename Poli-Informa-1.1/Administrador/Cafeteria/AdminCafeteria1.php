146d6537d96892466e318e93b3e39bc295b5b410
ralpe <ralpeduardofrias@gmail.com>
2024-03-29 22:47:44 -0600

    Carpeta de soporte y de usuarios puestas

--- Poli-Informa-1.1/Administrador/Cafeteria/AdminCafeteria1.php
+++ Poli-Informa-1.1/Administrador/Cafeteria/AdminCafeteria1.php
@@ -12,7 +12,7 @@ $conexion = $db->connect();
     <link rel="stylesheet" href="css/AdminCafeteria.css">
     <script src="js/AdminCafeteria.js"></script>
     <!-- Link separados (iconos) -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />    <title>Administracion Cafeteria</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>Administración Cafeteria</title>
 </head>
