87a424cbf21b2896d13aa3ff31a823e02a200622
kikethor2356 <enrique.alejandro2356amil.com>
2024-04-08 19:45:05 -0600

    Ya estasmos por finalizar bro

--- Poli-Informa-1.1/Cliente/Horarios/Horario.php
+++ Poli-Informa-1.1/Cliente/Horarios/Horario.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="footer-page/style.css"> 
+    <link rel="stylesheet" href="footer-page/style.css">
+    <link rel="stylesheet" href="../Partes/footer-page/style.css"> 
+
     <title>Bienvenido a la seccion de horarios</title>
 </head>
 
