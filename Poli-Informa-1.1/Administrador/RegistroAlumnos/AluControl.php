153d8f532b604fa2d5ad778fb29b9b2f749d0783
paola101012 <paolamarsan12@gmail.com>
2024-04-19 22:06:31 -0600

    Actualización Registro Alumno veta 19/04/24

--- Poli-Informa-1.1/Administrador/RegistroAlumnos/AluControl.php
+++ Poli-Informa-1.1/Administrador/RegistroAlumnos/AluControl.php
@@ -169,7 +169,7 @@ $conexion = $db->connect();
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         
-        <script src="JS/AdFunciones.js"></script>
+        <script src="JS/AluFunciones.js"></script>
     
         <!-- Agregar -->
             <?php
