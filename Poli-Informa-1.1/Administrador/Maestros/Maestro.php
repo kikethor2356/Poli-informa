c79febda942002b7b66bdb1f31147fdfe886c832
kikethor2356 <enrique.alejandro2356amil.com>
2024-05-02 18:18:45 -0600

    Cambio de color del perfil del maestro

--- Poli-Informa-1.1/Administrador/Maestros/Maestro.php
+++ Poli-Informa-1.1/Administrador/Maestros/Maestro.php
@@ -126,7 +126,7 @@ class Maestro extends Database
                 </style>
 
 
-                <section class="vh-100" style="background-color: black;">
+                <section class="vh-100">
                     <div class="container py-5 h-100">
                         <div class="row d-flex justify-content-center align-items-center h-100">
                             <div class="col col-lg-6 mb-4 mb-lg-0">
