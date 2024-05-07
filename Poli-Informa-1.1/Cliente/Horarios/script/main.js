f55137456e13c66309ec38c589dc83de39812958
kikethor2356 <enrique.alejandro2356amil.com>
2024-05-06 18:59:30 -0600

    Ya quedo el perfil del maestro ( solo en el cliente)

--- Poli-Informa-1.1/Cliente/Horarios/script/main.js
+++ Poli-Informa-1.1/Cliente/Horarios/script/main.js
@@ -9,3 +9,55 @@ document.querySelector('.a_Start').addEventListener('click', function (event) {
         behavior: 'smooth' // Opciones de desplazamiento suave
     });
 });
+
+
+
+// Obtener elementos del DOM
+var modal = document.getElementById("modal");
+var btnModal = document.getElementById("btnModal");
+var spanClose = document.getElementsByClassName("close")[0];
+
+// Función para mostrar la ventana modal
+btnModal.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Función para ocultar la ventana modal
+spanClose.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Ocultar la ventana modal cuando se haga clic fuera de ella
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+
+        // Obtén el modal
+        var modal = document.getElementById('modal');
+
+        // Obtén el botón que cierra el modal
+        var span = document.getElementsByClassName('close')[0];
+
+        // Cuando el usuario hace clic en <span> (x), cierra el modal
+        span.onclick = function() {
+            modal.classList.add('fadeout'); // Aplica la animación de cierre
+            setTimeout(function() {
+                modal.style.display = 'none';
+                modal.classList.remove('fadeout'); // Remueve la clase fadeout después de la animación
+            }, 500); // Espera 500 milisegundos (igual a la duración de la animación)
+        }
+
+        // Cuando el usuario hace clic en cualquier lugar fuera del modal, ciérralo
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.classList.add('fadeout'); // Aplica la animación de cierre
+                setTimeout(function() {
+                    modal.style.display = 'none';
+                    modal.classList.remove('fadeout'); // Remueve la clase fadeout después de la animación
+                }, 500); // Espera 500 milisegundos (igual a la duración de la animación)
+            }
+        }
